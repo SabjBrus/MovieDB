eae8cf3d07a20a1684ea395c95924b7add685695
Борис Жуков <boris10264@gmail.com>
2023-03-29 18:03:12 +0300

    add en locale

--- movies/admin.py
+++ movies/admin.py
@@ -1,15 +1,18 @@
 from django.contrib import admin
 from django import forms
 from django.utils.safestring import mark_safe
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+from modeltranslation.admin import TranslationAdmin
 
 from movies.models import Category, Genre, Movie, MovieShots, Actor, Rating, RatingStar, Reviews
 
 
-from ckeditor_uploader.widgets import CKEditorUploadingWidget
-
-
 class MovieAdminForm(forms.ModelForm):
-    description = forms.CharField(
+    description_ru = forms.CharField(
+        label='Описание',
+        widget=CKEditorUploadingWidget(),
+    )
+    description_en = forms.CharField(
         label='Описание',
         widget=CKEditorUploadingWidget(),
     )
@@ -20,7 +23,7 @@ class MovieAdminForm(forms.ModelForm):
 
 
 @admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
+class CategoryAdmin(TranslationAdmin):
     """Категории"""
     list_display = ('id', 'name', 'url',)
     list_display_links = ('name',)
@@ -45,7 +48,7 @@ class MovieShortsInline(admin.TabularInline):
 
 
 @admin.register(Movie)
-class MovieAdmin(admin.ModelAdmin):
+class MovieAdmin(TranslationAdmin):
     """Фильмы"""
     list_display = ('title', 'category', 'url', 'draft',)
     list_filter = ('category', 'year',)
@@ -117,13 +120,13 @@ class ReviewAdmin(admin.ModelAdmin):
 
 
 @admin.register(Genre)
-class GenreAdmin(admin.ModelAdmin):
+class GenreAdmin(TranslationAdmin):
     """Жанры"""
     list_display = ('name', 'url')
 
 
 @admin.register(Actor)
-class ActorAdmin(admin.ModelAdmin):
+class ActorAdmin(TranslationAdmin):
     """Актеры"""
     list_display = ('name', 'age', 'get_image')
     readonly_fields = ('get_image', )
@@ -141,7 +144,7 @@ class RatingAdmin(admin.ModelAdmin):
 
 
 @admin.register(MovieShots)
-class MovieShotsAdmin(admin.ModelAdmin):
+class MovieShotsAdmin(TranslationAdmin):
     """Кадры из фильма"""
     list_display = ('title', 'movie', 'get_image')
     readonly_fields = ('get_image',)
